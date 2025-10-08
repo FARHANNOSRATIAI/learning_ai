439c702fb1a6539fa68df8254f7438f00f3fc19b
farhan nosrati <nosratidevelopments@gmail.com>
2025-10-08 09:07:33 -0700

    Add files via upload

---
+++ python basic.py
@@ -0,0 +1,16 @@
+import number
+
+if a = 5
+b = 3
+c = a + b
+print(c)
+else: b < a
+
+print (" the b is lower than a")
+
+git init
+git add .
+git commit -m "my first python exercises"
+git branch -M main
+git remote add origin https://github.com/FARHANNOSRATIAI/learning_ai.git
+git push -u origin main
